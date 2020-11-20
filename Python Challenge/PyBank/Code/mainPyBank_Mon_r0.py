0555531eb10d2e2d666a8dfa7dda89eacdbd4f7c
troyyoungblood <72400175+troyyoungblood@users.noreply.github.com>
2020-11-19 22:06:52 -0600

    adding folders and files to git hub location

---
+++ Python Challenge/PyBank/Code/mainPyBank_Mon_r0.py
@@ -0,0 +1,93 @@
+#Establish Modules
+import os
+import csv
+
+#Create path to source file
+#bankdatapath = os.path.join("..", "Resources", "budget_data.csv")
+bankdatapath = "C:/Users/troyy/Desktop/Class_Material/TY_Homework_Folder/20201121_Homework_3/Python_Challenge/PyBank/Resources/budget_data.csv"
+
+#Create variable
+nettotal_pl = []         #Net total Profit(P)/Loss(L) over entire period 
+allmonths = []           #List of all the months
+avgcumdiff = float()     #Average difference in P/L
+cumdiff = []             #List of changes between P/L
+rowdiff = float()        #Difference between successive P/L  
+greatinc_p = int()       #Greatest increase in P over entire period 
+greatdec_l = int()       #Greatest decrease in losses over entire period 
+monthdiff = {}           #Dictionary that will hold dates and changes
+
+#Read source file into Python environment
+with open(bankdatapath) as bankdatacsv: 
+    bankdatareader = csv.reader(bankdatacsv, delimiter = ",")
+   
+    #move past header row
+    next(bankdatareader)
+
+    for row in bankdatareader:
+        
+        #Steps to strip off profit and loses into dedicated list
+        plvalue = int(row[1])
+        nettotal_pl.append(plvalue)
+        #Steps to strip off dates in list into dedicated list
+        monthvalue = (row[0])
+        allmonths.append(monthvalue)
+    
+#Determine total number of months
+totalmonths = len(nettotal_pl)
+
+#Caluculate changes in P/L over period, then find max, min and average
+a = 0
+b = 1
+
+for a in range(len(nettotal_pl)):
+    if b < len(nettotal_pl):
+        rowdiff = nettotal_pl[b] - nettotal_pl[b-1]
+        b = b + 1
+        cumdiff.append(rowdiff)
+#print(cumdiff)
+avgcumdiff = sum(cumdiff) / len(cumdiff)
+#Format avgcumdiff to 2 decimals
+formatted_avgcumdiff = "{:.2f}".format(avgcumdiff)
+#print(formatted_avgcumdiff)
+
+#Create dictionary of allmonths and cumdiff - this will be used to match max values with month
+#Make lists same length.  cumdiff is shorter and needs 0 at beginning
+placeholder = 0
+cumdiff.insert(0,placeholder)
+
+#Zipping two lists together to create dictionary monthdiff
+zip_iterator = zip(allmonths, cumdiff)
+monthdiff = dict(zip_iterator)
+
+#Obtaining max increase change from dictionary monthdiff
+greatinc_p = max(monthdiff.values())
+#Obtaining max increase key from dictionary monthdiff
+gim_key = [keys for keys, values in monthdiff.items() if values == max(monthdiff.values())]
+
+#Obtaining max (min) decrease from dictionary monthdiff
+greatdec_l = min(monthdiff.values())
+#Octaining max (min) decrease key from dictionary monthdiff
+gdm_key = [keys for keys, values in monthdiff.items() if values == min(monthdiff.values())]
+
+
+print(f'Total Months: {totalmonths}')
+print("Total: $",sum(nettotal_pl))
+print(f'Average Change: $ {formatted_avgcumdiff}')
+print(f'Greatest Increase in Profits:  {gim_key[0]} $ {greatinc_p}')
+print(f'Greatest Decrease in Profits:  {gdm_key[0]} $ {greatdec_l}')
+
+#Create output path to final analysis file - final_analysis_tdy.txt
+#output_path = os.path.join("..", "Analysis", "final_analysis_tdy.txt")
+output_path = "C:/Users/troyy/Desktop/Class_Material/TY_Homework_Folder/20201121_Homework_3/Python_Challenge/PyBank/Analysis/final_analysis_tdy.txt"
+with open(output_path, "w") as text_file:
+    print(f'Total Months: {totalmonths}', file = text_file)
+    print("Total: $",sum(nettotal_pl), file = text_file)
+    print(f'Average Change: $ {formatted_avgcumdiff}', file = text_file)
+    print(f'Greatest Increase in Profits: {gim_key[0]}  $ {greatinc_p}', file = text_file)
+    print(f'Greatest Decrease in Profits: {gdm_key[0]}  $ {greatdec_l}', file = text_file)
+
+
+
+
+ 
+
---
+++ Python Challenge/PyPoll/Code/mainPyPoll_r2.py
@@ -0,0 +1,90 @@
+#Establish Modules
+import os
+import csv
+import collections  #new module import - used for counting
+
+#Create path to source file
+polldatapath = os.path.join("..", "Resources", "election_data.csv")
+#polldatapath = "C:/Users/troyy/Desktop/Class_Material/TY_Homework_Folder/20201121_Homework_3/Python_Challenge/PyPoll/Resources/election_data.csv"
+
+#Create variable 
+allvotescast = []      #List of votes cast 
+totalvotes = ()        #Total number of votes cast
+candidates = []        #List of candidates
+perlist = []           #List of percentages
+truncperlist = []      #Truncated percent list
+valuelist = []         #List of votes
+votecounter_dict = {}  #Dictionary with candidate and their respective votes
+
+#Read source file into Python environment
+with open(polldatapath,'r') as polldatacsv: 
+    polldatareader = csv.reader(polldatacsv, delimiter = ",")
+    
+    #move past header row
+    next(polldatareader)
+
+    for row in polldatareader:
+        
+        #Steps to create list with votes cast. stripping out votes (candidates) from data file
+        eachvotecast = (row[2])
+        #Add amend allvotescast with eachvotecast - this is the list with all the votes  
+        allvotescast.append(eachvotecast)
+
+#Determine total votes cast
+totalvotes = len(allvotescast)
+
+#Identifying candidates and counting votes received, using "Counter" from "collections" 
+# function creates paired elements - a key(candidate name) and value (votes received).  
+# The function counts and provides votes received (values) for each unique candidate name (key)
+vote_counter = collections.Counter(allvotescast)
+
+#Calculating percentages for each candidate
+#Convert a "counter" to a "dictionary" for doing calculation
+votecounter_dict = dict(vote_counter)
+print(votecounter_dict)
+
+#Getting Candidates percent votes and votes lists 
+for value in votecounter_dict.keys():
+    percent = (votecounter_dict[value] * 100.0) / totalvotes
+    # this is list of candidates
+    candidates.append(value)
+    # this is list of percentage won by each candidate
+    perlist.append(percent)
+
+for values in votecounter_dict.values():
+    # this list has the number of votes cast for each candidate
+    valuelist.append(values)
+
+#Truncate perlist to 3 decimals
+truncperlist = ['{:.3f}'.format(elem) for elem in perlist]
+
+#Obtaining winning candidate key from dictionary votecounter_dict
+wincan_key = [keys for keys, values in votecounter_dict.items() if values == max(votecounter_dict.values())]
+
+print(f'Election Results')
+print('----------------------------')
+print(f'Total Votes: {totalvotes}')
+print('----------------------------')
+print(f' {candidates[0]}     : {truncperlist[0]}% {valuelist[0]}')
+print(f' {candidates[1]}   : {truncperlist[1]}% {valuelist[1]}')
+print(f' {candidates[2]}       : {truncperlist[2]}% {valuelist[2]}')
+print(f' {candidates[3]} : {truncperlist[3]}%  {valuelist[3]}')
+print('----------------------------')
+print(" Winner:", (*wincan_key))   #(*wincan_key) - the star removes (unpacks) extra stuff
+print('----------------------------')
+
+#Create output path to final analysis file - final_poll_tdy.txt
+#output_path = os.path.join("..", "Analysis", "final_poll_tdy.txt")
+output_path = "C:/Users/troyy/Desktop/Class_Material/TY_Homework_Folder/20201121_Homework_3/Python_Challenge/PyPoll/Analysis/final_poll_tdy.txt"
+with open(output_path, "w") as text_file:
+    print(f'Election Results', file = text_file)
+    print('----------------------------', file = text_file)
+    print(f'Total Votes: {totalvotes}', file = text_file)
+    print('----------------------------', file = text_file)
+    print(f' {candidates[0]}     : {truncperlist[0]}% {valuelist[0]}', file = text_file)
+    print(f' {candidates[1]}   : {truncperlist[1]}% {valuelist[1]}', file = text_file)
+    print(f' {candidates[2]}       : {truncperlist[2]}% {valuelist[2]}',file = text_file)
+    print(f' {candidates[3]} : {truncperlist[3]}%  {valuelist[3]}', file = text_file)
+    print('----------------------------', file = text_file)
+    print(" Winner:", (*wincan_key), file = text_file)   #(*wincan_key) - the star removes (unpacks) extra stuff
+    print('----------------------------', file = text_file)
